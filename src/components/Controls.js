02cb6212aea4c4eebe2fdccbd2519f77c8fb0be9
Yazidn <secho771@gmail.com>
2018-12-21 19:46:23 +0100

    deleted a lot of unnecessary comments

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './Controls.css';
-// import logo from './logo.png';
 
 class Controls extends Component {
     render() {
         return (
             <div className="control-bar">
-                {/* <img id="logo" src={logo} alt="logo"/> */}
                 <button title="Add a new card" className="create-button" onClick={this.props.addOnClick}><i className="fas fa-sticky-note"></i></button>
                 <button title="Remove a card" className="edit-button" onClick={this.props.editToggle}><i className="fas fa-trash-alt"></i></button>
                 <button title="Backup the list cards" className="backup-button" onClick={this.props.backup}><i className="fas fa-download"></i></button>
--- src/components/colorPicker.js
+++ src/components/colorPicker.js
@@ -5,11 +5,7 @@ class ColorPicker extends Component {
 
     renderColorPicker() {
         let colors = ['#ff4141', '#68994f', '#3a89a8', '#ffcc42', '#856fdb', '#ea64da', '#ed8a63', '#f79a2a', '#9bf27b'];
-
-        
-
         let colorChoices = [];
-
         for (let i = 0; i < colors.length; i++) {
             colorChoices.push(
                 <div key={i} onClick={this.props.setColor} className="color-div" style={{
